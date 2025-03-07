5a587e6a7dfecc19f500d72458e6796b0d351c2e
lumexonline <lumexmumbai24@gmail.com>
2025-03-07 12:33:43 +0530

    Update analytics.js

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,7 +1,7 @@
 
 import ReactGA from 'react-ga4';
 
-const GA_MEASUREMENT_ID = 'G-XXXXXXXXXX'; // Replace with your actual GA4 Measurement ID
+const GA_MEASUREMENT_ID = 'G-6NH1BTWM64'; // Replace with your actual GA4 Measurement ID
 
 export const initGA = () => {
   ReactGA.initialize(GA_MEASUREMENT_ID);
